31a769c21dd72469c0ce4458706c73cc8f68e5ed
harshankur <harshankur@outlook.com>
2019-05-02 20:05:01 +0900

    initial module

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # anyFileParser
 Node.JS library that can parse text from any kind of file, eg. text, image, pdf, office files.
+
+
+#### Update
+* 2019/05/02 - Initial module with pdf, image, utf-8 and office files.
+
+
+
+## Install via npm
+
+
+```
+npm i anyfileparser
+```
+
+----------
+
+**Usage**
+```
+const anyFileParser = require('anyfileparser');
+
+anyFileParser.parseFile("/path/to/file", function(data){
+        // "data" string in the callback here is the text parsed from the file passed in the first argument above
+        console.log(data)
+})
+
+```
+
+**Example**
+```
+const anyFileParser = require('anyfileparser');
+
+anyFileParser.parseFile("C:\\files\\myText.docx", function(data){
+        var newText = data + "look, I can parse any file"
+        callSomeOtherFunction(newText);
+})
+
+// Using relative path for file is also fine
+anyFileParser.parseFile("files/myWorkSheet.ods", function(data){
+        var newText = data + "look, I can parse any file"
+        callSomeOtherFunction(newText);
+})
+```
+
--- anyFileParser.js
+++ anyFileParser.js
@@ -1,15 +1,37 @@
 const fs = require('fs')
-const util = require('util');
 const Tesseract = require('tesseract.js');
+const pdf = require('pdf-parse');
+const officeParser = reqiore('officeparser');
 
 
+async function parseFile(filename, callback) {
+    var myExtension = filename.split('.').pop().toLowerCase();
 
-
-
-async function anyFileParser(filename, callback) {
-    
+    if(myExtension == "bmp" || myExtension == "pnm" || myExtension == "png" || myExtension == "jfif" || myExtension == "jpg" || myExtension == "jpeg" || myExtension == "tiff") {
+        Tesseract.recognize(filename)
+        .finally((result) => {
+            callback(result);
+        })
+    }
+    else if(myExtension == "pdf") {
+        var dataBuffer = await fs.readFileSync(filename);
+        
+        pdf(dataBuffer)
+        .then((data) => {
+            callback(data.text);
+        })
+    }
+    else if(myExtension == "docx" || myExtension == "pptx" || myExtension == "xlsx" || myExtension == "odt" || myExtension == "odp" || myExtension == "ods") {
+        officeParser.parseOffice(filename, (result) => {
+            callback(result);
+        })
+    }
+    else {
+        callback(await fs.readFileSync(location, 'utf-8').toString());
+    }
 }
 
 
+module.exports.parseFile = parseFile;
 
 
